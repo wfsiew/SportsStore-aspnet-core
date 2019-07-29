89c7850b6e12164977cbada6d454cdaa1edd5690
Siew Wing Fei <siewwingfei@hotmail.com>
2019-07-29 09:37:26 +0800

    j

---
+++ sportsstore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using sportsstore.Models;
+
+namespace sportsstore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
